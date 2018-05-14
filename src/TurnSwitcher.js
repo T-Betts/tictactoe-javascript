3fe4f23bc09c5f16f36b8f577ea682813ee99eee
Tom Betts <tom.betts@live.co.uk>
2018-05-14 15:43:52 +0100

    Add getCurrentTurn function

--- src/TurnSwitcher.js
+++ src/TurnSwitcher.js
@@ -2,6 +2,10 @@ function TurnSwitcher() {
   this.currentTurn = 'X'
 };
 
+TurnSwitcher.prototype.getCurrentTurn = function () {
+  return this.currentTurn
+};
+
 TurnSwitcher.prototype.switchTurn = function() {
   this.currentTurn == 'X' ? this.currentTurn = 'O' : this.currentTurn = 'X'
 };
