5b0c38cfe2e69ce50b3633c2cbd4038d957c74e4
Tom Betts <tom.betts@live.co.uk>
2018-05-14 15:13:33 +0100

    Pass test for showBoard function

--- src/Game.js
+++ src/Game.js
@@ -1,1 +1,12 @@
+function Game() {
+  this.board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+};
 
+Game.prototype.showBoard = function() {
+  var rows = [];
+  var array = this.board;
+  for (var i = 0; i < array.length; i+=3) {
+    rows.push([array[i], array[i + 1], array[i + 2]])
+  }
+  return rows;
+};
