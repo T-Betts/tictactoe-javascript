d3e059c2a0cf276bc18fca4c7c9f90e93d364e94
Tom Betts <tom.betts@live.co.uk>
2018-05-14 16:29:39 +0100

    Add test that checks place works for turn 'o'

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,11 +1,16 @@
 describe('Game', function(){
 
   let xTurn;
+  let oTurn;
 
   beforeEach(function(){
     newGame = new Game(xTurn);
+    oMoveGame = new Game(oTurn)
+
     xTurn = jasmine.createSpyObj('xTurn',['getCurrentTurn']);
     xTurn.getCurrentTurn.and.returnValue("X");
+    oTurn = jasmine.createSpyObj('oTurn',['getCurrentTurn']);
+    oTurn.getCurrentTurn.and.returnValue("O");
   });
 
   describe('showBoard', function(){
@@ -21,9 +26,16 @@ describe('Game', function(){
   });
 
   describe('place', function(){
-    it('can place symbol of current player in designated square', function(){
+    it('can place "X" in designated square when currentTurn is "X"', function(){
       newGame.place(4)
       expect(newGame.showBoard()).toEqual([['-', '-', '-'], ['-', 'X', '-'], ['-', '-', '-']])
+    });
+  });
+
+  describe('place', function(){
+    it('can place "O" in designated square when currentTurn is "O"', function(){
+      oMoveGame.place(4)
+      expect(oMoveGame.showBoard()).toEqual([['-', '-', '-'], ['-', 'O', '-'], ['-', '-', '-']])
     });
   });
 });
