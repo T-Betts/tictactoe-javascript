f7fae329af09f88538657dd986a1bdb705a256c2
Tom Betts <tom.betts@live.co.uk>
2018-05-14 16:25:36 +0100

    Refactor GameSpec

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -3,29 +3,27 @@ describe('Game', function(){
   let xTurn;
 
   beforeEach(function(){
+    newGame = new Game(xTurn);
     xTurn = jasmine.createSpyObj('xTurn',['getCurrentTurn']);
     xTurn.getCurrentTurn.and.returnValue("X");
   });
 
   describe('showBoard', function(){
     it('shows the current state of the board', function(){
-      game = new Game
-      expect(game.showBoard()).toEqual([['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']])
+      expect(newGame.showBoard()).toEqual([['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']])
     });
   });
 
   describe('turn', function(){
     it('shows whose turn it is', function(){
-      game = new Game(xTurn)
-      expect(game.turn).toEqual("X")
+      expect(newGame.turn).toEqual("X")
     });
   });
 
   describe('place', function(){
     it('can place symbol of current player in designated square', function(){
-      game = new Game(xTurn)
-      game.place(4)
-      expect(game.showBoard()).toEqual([['-', '-', '-'], ['-', 'X', '-'], ['-', '-', '-']])
+      newGame.place(4)
+      expect(newGame.showBoard()).toEqual([['-', '-', '-'], ['-', 'X', '-'], ['-', '-', '-']])
     });
   });
 });
