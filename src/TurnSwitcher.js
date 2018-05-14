9fbd35bf26af4569c3221dbdc4862ec0ddfd5a38
Tom Betts <tom.betts@live.co.uk>
2018-05-14 13:21:06 +0100

    Pass switchTurn tests.

--- src/TurnSwitcher.js
+++ src/TurnSwitcher.js
@@ -1,7 +1,7 @@
-function TurnSwitcher(){
+function TurnSwitcher() {
   this.currentTurn = 'X'
 };
 
-TurnSwitcher.prototype.switchTurn = function () {
-  this.currentTurn = 'O'
+TurnSwitcher.prototype.switchTurn = function() {
+  this.currentTurn == 'X' ? this.currentTurn = 'O' : this.currentTurn = 'X'
 };
