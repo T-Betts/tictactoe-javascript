4c3777f173fb1ffda5c0d7053260b58c9e5e090b
Tom Betts <tom.betts@live.co.uk>
2018-05-15 13:55:02 +0100

    Write test that expects isGameWon() to return false when no winning lines on board

--- spec/GameStatusCheckerSpec.js
+++ spec/GameStatusCheckerSpec.js
@@ -1,5 +1,6 @@
 describe('GameStatusChecker', function(){
   let victoryGame;
+  let noVictoryGame;
 
   beforeEach(function(){
     gameStatusChecker = new GameStatusChecker;
@@ -7,11 +8,18 @@ describe('GameStatusChecker', function(){
     victoryGame = jasmine.createSpyObj('victoryGame',['returnBoard', 'showTurn']);
     victoryGame.showTurn.and.returnValue('X');
     victoryGame.returnBoard.and.returnValue(['X', '-', '-', '-', 'X', '-', '-', '-', 'X'])
+    noVictoryGame = jasmine.createSpyObj('noVictoryGame',['returnBoard', 'showTurn']);
+    noVictoryGame.showTurn.and.returnValue('X');
+    noVictoryGame.returnBoard.and.returnValue(['O', 'X', 'O', '-', 'X', '-', '-', '-', 'X'])
   });
 
   describe('isGameWon', function(){
     it('should return true if game has been won', function(){
       expect(gameStatusChecker.isGameWon(victoryGame)).toEqual(true)
+    });
+
+    it('should return false if the game has not been won', function(){
+      expect(gameStatusChecker.isGameWon(noVictoryGame)).toEqual(false)
     });
   });
 });
