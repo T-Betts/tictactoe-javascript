5d0f2295634159bd4fc4c38350bb4b247f8aae5e
Tom Betts <tom.betts@live.co.uk>
2018-05-15 13:35:20 +0100

    Add 'showTurn()' function to Game.js

--- src/Game.js
+++ src/Game.js
@@ -16,6 +16,10 @@ Game.prototype.returnBoard = function () {
   return this.board
 };
 
+Game.prototype.showTurn = function () {
+  return this.turn
+};
+
 Game.prototype.place = function (square) {
   if (this.board[square] != "-"){
     throw "Square already taken."
