d6d551f64a8edd89b0c13ac0071e71f2f6e01939
Tom Betts <tom.betts@live.co.uk>
2018-05-17 19:14:35 +0100

    Write test that expects makeMove to place 'X' in designated square.

---
+++ spec/GameSpec.js
@@ -0,0 +1,9 @@
+describe('Game', function(){
+  describe('makeMove', function(){
+    it('runs place function for a designated square', function(){
+      game = new Game
+      game.makeMove(4)
+      expect(game.returnBoard()[4]).toEqual('X')
+    });
+  });
+});
