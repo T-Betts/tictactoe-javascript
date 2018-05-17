e3f8f8f857b97482c4e1b1c9b0b5d395709d5f15
Tom Betts <tom.betts@live.co.uk>
2018-05-17 19:21:55 +0100

    Write test that expects makeMove() to switch turns if symbol was placed

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,9 +1,18 @@
 describe('Game', function(){
+
+  beforeEach(function(){
+    game = new Game
+  });
+
   describe('makeMove', function(){
     it('runs place function for a designated square', function(){
-      game = new Game
       game.makeMove(4)
       expect(game.board.returnBoard()[4]).toEqual('X')
+    });
+
+    it('switches the turn if a symbol was placed', function(){
+      game.makeMove(4)
+      expect(game.board.turnswitcher.getCurrentTurn()).toEqual('O')
     });
   });
 });
